8261c593d54b5075c9d49ed32528e9688f3bb482
Konsti <kklvau@t-online.de>
2021-01-26 13:45:33 +0100

    added Input funtion and SaveLocation funtion

--- pdf_merger/script.py
+++ pdf_merger/script.py
@@ -2,31 +2,44 @@
 #imports
 ########
 from PyPDF2 import PdfFileReader, PdfFileMerger
+from tkinter import Tk
+from tkinter.filedialog import askopenfilenames, asksaveasfilename
+
+########
+#Variables
+########
+
 
 ########
 #script
 ########
 
+##########################################################################
 def main():
     """
     """
-
     pdfOut = PdfFileMerger()
     pdfIn = pdfInput()
-    save = saveLocation()
-
     for elem in pdfIn:
         pdfOut.append(elem)
-    
+    save = saveLocation()
     pdfOut.write(save)
-
+##########################################################################
 def pdfInput():
     """
     """
+    Tk().withdraw()
+    files = askopenfilenames(title="Choose files", filetypes =[('pdf file', '*.pdf')])
+    return files
 
+
+##########################################################################
 def saveLocation():
     """
     """
+    Tk().withdraw()
+    fileName = asksaveasfilename(title='Choose a filename', defaultextension='.pdf', filetypes =[('pdf file', '*.pdf')])
+    return fileName
 
 
 if __name__ == "__main__":
