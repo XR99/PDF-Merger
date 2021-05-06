6f81ac6d3e94709df45cf101f6c6611213a7daaa
Konsti <kklvau@t-online.de>
2021-05-06 14:41:13 +0200

    provided functions for cli options

--- src/pdf_merger/pdfMerger.py
+++ src/pdf_merger/pdfMerger.py
@@ -16,9 +16,11 @@ from tkinter.filedialog import askopenfilenames, asksaveasfilename
 ########
 
 ##########################################################################
-def main():
+def main(*pdfDocs):
     """
-    Merge pdfs a certain way based on given flag
+    Coordinate merge of pdf documents
+        process on basis of given flag
+        process on basis of provided documents in argument pdfDocs
     """
     parser = argparse.ArgumentParser(description='Merge pdf documents.')
     parser.add_argument("-g", "--gui", help="provides a graphical user interface", action="store_true")
@@ -26,31 +28,40 @@ def main():
     args = parser.parse_args()
 
     if args.gui:
-        """
-        Merge pdfs guided with gui filechooser
-        """
-        pdfIn = pdfInput()
-        if len(pdfIn) < 2:
-            print("You need to provide at least 2 Pdf documents!")
-            return
-        pdfOut = saveLocation()
-        if pdfOut is None:
-            print("You need to provide a save location!")
-            return
-        mergedPdf = merge(pdfIn)
-        if merge is None:
-            print("Error in merge occured!")
-            return
-        save(mergedPdf, pdfOut)
-        print("Merge was successful")
+        value = optGui()
+        print(value)
         return
     elif args.directory:
-        """
-        Merge all pdfs in a given directory
-        """
-        print("provided directory: {}".format(args.directory))
+        value = optDirectory(args.directory)
+        print(value)
         return
-
+##########################################################################
+def optGui():
+    """
+    Merge pdfs guided with gui filechooser
+    """
+    pdfIn = pdfInput()
+    if len(pdfIn) < 2:
+        msg = "You need to provide at least 2 Pdf documents!"
+        return msg
+    pdfOut = saveLocation()
+    if pdfOut is None:
+        msg = "You need to provide a save location!"
+        return 1
+    mergedPdf = merge(pdfIn)
+    if merge is None:
+        msg = "Error in merge occured!"
+        return msg
+    save(mergedPdf, pdfOut)
+    msg = "Merge was successful"
+    return msg
+##########################################################################
+def optDirectory(directory):
+    """
+    Merge all pdfs in a given directory
+    """
+    msg = "provided directory: {}".format(directory)
+    return msg
 ##########################################################################
 def merge(input):
     """
